e59f1985b332c7145e97899e753641ce00ee8c40
Jasvir Virdi <44513117+jvirdi2@users.noreply.github.com>
2019-07-03 12:26:38 -0500

    Add files via upload

---
+++ bubble_sort.py
@@ -0,0 +1,18 @@
+
+# Simple Bubble sort implementation
+# Pushes biggest element to the extreme right side by swapping adjacent elements
+
+def bubble(A):
+    len_A=len(A)
+    for i in range(0,len_A):
+        for j in range(0,len_A-1-i):
+            if A[j]>A[j+1]:
+                temp_1=A[j]
+                temp_2=A[j+1]
+                A[j]=temp_2
+                A[j+1]=temp_1
+    return A
+
+A=[2,1,3,2,0,-1]
+print (bubble(A))
+    
