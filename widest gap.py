ac1ea2af2fa570dcfa2e5871422f1fd3458e6b5f
Jasvir Virdi <44513117+jvirdi2@users.noreply.github.com>
2020-02-11 18:21:47 -0600

    Update widest gap.py

--- widest gap.py
+++ widest gap.py
@@ -23,35 +23,31 @@ def widest_gap(n,start,finish):
                 lane_list.remove(j)
         except:
             continue
-
+    print(lane_list)
     
 # Below code, Helps in finding sub arrays within the main array, having successive elments. 
-    lane_list.append(-1)
     len_lane_list=len(lane_list)
-    
-    gap=1
-    list_of_gaps=[]
-    
     if len_lane_list==0:
             return 0
     else:
-        for i in range(0,len_lane_list-1):
-            if lane_list[i+1]==(1+lane_list[i]):
-                gap=gap+1
-                    
-                if (i+1)==(len_lane_list-1):
-                        list_of_gaps.append(gap)
-            else:
-                list_of_gaps.append(gap)
-                gap=1
-    print(list_of_gaps)              
-    return max(list_of_gaps)
+        lane_list.append(-1)
+        gap=1
+        list_of_gaps=[]
+        for i in range(0,len_lane_list):
+                if lane_list[i+1]==(1+lane_list[i]):
+                    gap=gap+1
+                else:
+                    list_of_gaps.append(gap)
+                    gap=1
+        print(list_of_gaps)              
+        return max(list_of_gaps)
 
         
 n=10
 #start=[8,2,5,1]
 #finish=[10,2,6,2]
-start=[3,8,5]
-finish=[3,10,7]
-
+#start=[3,8,5]
+#finish=[3,10,7]
+start=[1,7]
+finish=[5,10]
 print(widest_gap(n,start,finish))
